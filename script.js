dd130c46d4b54e12ecb5077a789c80eb94ed238d
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-11-08 11:25:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 let currentLoc = location.href;
 let urlLength = currentLoc.length;
-alert(`The length of the URL is ${urlLength}`)
+alert(`The length of the URL is: ${urlLength}`);
